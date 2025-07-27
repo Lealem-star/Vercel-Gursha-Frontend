aec1752ba369f5161efd8db4690620e671a91dcb
Lealem Meseret <meseretlealem8@gmail.com>
2025-07-27 10:26:53 +0300

    correct route in frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://vercel-gursha-backend-3.vercel.app/api';
 
 // Create axios instance with base configuration
 const api = axios.create({
